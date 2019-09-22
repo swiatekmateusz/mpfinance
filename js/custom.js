fbe27086344dd7c31c0f42f26830613dc661bba8
swiatekmateusz <swiatek.mateusz.m@gmail.com>
2019-09-23 00:15:55 +0200

    Do pokazania

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,10 @@ $('.mainslider').slick({
   autoplaySpeed: 2000,
   arrows: true,
 });
+
+$('.cooperationsslider').slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
